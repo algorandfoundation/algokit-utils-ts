8c56022f337001c3becd79c7b06b3cb53d812b8e
Rob Moore <rob.moore@makerx.com.au>
2023-03-09 23:05:17 +0800

    build: Fixing incorrect config

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,6 @@ module.exports = {
     // Allow sentence case commit messages
     'subject-case': [2, 'never', ['pascal-case', 'upper-case']],
     'type-empty': [1, 'never'],
-    'body-leading-blank': 'off',
+    'body-leading-blank': ['off'],
   },
 }
