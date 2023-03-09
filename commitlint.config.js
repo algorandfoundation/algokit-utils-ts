c6283a9906eaddbc3d468aa649aa49726d673b88
Rob Moore <rob.moore@makerx.com.au>
2023-03-09 22:50:52 +0800

    chore: Update commit rules

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,3 +1,8 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Allow sentence case commit messages
+    'subject-case': [2, 'never', ['pascal-case', 'upper-case']],
+    'type-empty': [1, 'never'],
+  },
 }
