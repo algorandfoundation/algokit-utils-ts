29b9e10be8c475ea989d8882214077cd2577e646
Rob Moore <rob.moore@makerx.com.au>
2023-03-13 19:46:26 +0800

    build: Relaxing commitlint

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,6 +4,7 @@ module.exports = {
     // Allow sentence case commit messages
     'subject-case': [2, 'never', ['pascal-case', 'upper-case']],
     'type-empty': [1, 'never'],
+    'subject-empty': [1, 'always'],
     'body-leading-blank': [0, 'always'],
     'header-max-length': [1, 'always', 120],
   },
