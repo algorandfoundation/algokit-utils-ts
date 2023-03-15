53406d315db4370409aae28df31d9545eb29d81b
Rob Moore <rob.moore@makerx.com.au>
2023-03-15 18:15:11 +0800

    chore: Exporting application-client from /

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export * from './account'
 export * from './algo-amount'
 export * from './app'
+export * from './application-client'
 export * from './config'
 export * from './deploy-app'
 export * from './indexer-lookup'
