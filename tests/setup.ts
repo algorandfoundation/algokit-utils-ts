c60b0a3c54aa12b5a209a880a4ad7909d984799a
Daniel McGregor <daniel.mcgregor@makerx.com.au>
2023-04-28 15:38:10 +0800

    test: add dotenv to testing infrastructure

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,5 +1,17 @@
+import dotenv from 'dotenv'
+import fs from 'fs'
+import path from 'path'
 import { Config } from '../src/'
 import { consoleLogger } from '../src/types/logging'
+
+let envPath = path.resolve(path.join(__dirname, '..', '.env'))
+if (!fs.existsSync(envPath)) {
+  envPath = path.resolve(path.join(__dirname, '..', 'example.env'))
+}
+console.log(`Loading ${envPath}`)
+dotenv.config({
+  path: envPath,
+})
 
 Config.configure({
   logger: consoleLogger,
