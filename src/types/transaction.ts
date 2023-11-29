a3e69ec435aef5e54bf1965209a1f8e7bbb25120
Joe Polny <joepolny@gmail.com>
2023-11-29 09:14:35 -0500

    update packResources comment

--- src/types/transaction.ts
+++ src/types/transaction.ts
@@ -41,7 +41,7 @@ export interface SendTransactionParams {
   maxFee?: AlgoAmount
   /** The maximum number of rounds to wait for confirmation, only applies if `skipWaiting` is `undefined` or `false`, default: wait up to 5 rounds */
   maxRoundsToWaitForConfirmation?: number
-  /** Whether to use simulate to automatically pack app call resource into the txn objects */
+  /** Whether to use simulate to automatically pack app call resources into the txn objects. Defaults to true when there are app calls in the group.  */
   packResources?: boolean
 }
 
