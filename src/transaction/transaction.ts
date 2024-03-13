e323559660071eb96125c340a5e9fa064a1a336a
Rob Moore (MakerX) <rob.moore@makerx.com.au>
2024-03-13 19:24:25 +0800

    fix: remove all duplicate resources in resource population (#243)
    * fix: double addr in resource population
    
    * fix: remove all duplicate resources
    
    * docs
    
    ---------
    
    Co-authored-by: Joe Polny <joepolny@gmail.com>

--- src/transaction/transaction.ts
+++ src/transaction/transaction.ts
@@ -359,12 +359,20 @@ export async function populateAppCallResources(atc: algosdk.AtomicTransactionCom
       const txnIndex = findTxnBelowRefLimit(group, 'appLocal')
       group[txnIndex].txn.appForeignApps?.push(Number(a.app))
       group[txnIndex].txn.appAccounts?.push(algosdk.decodeAddress(a.account))
+
+      // Remove resources from the group if we're adding them here
+      g.accounts = g.accounts?.filter((acc) => acc !== a.account)
+      g.apps = g.apps?.filter((app) => BigInt(app) !== BigInt(a.app))
     })
 
     g.assetHoldings?.forEach((a) => {
       const txnIndex = findTxnBelowRefLimit(group, 'assetHolding')
       group[txnIndex].txn.appForeignAssets?.push(Number(a.asset))
       group[txnIndex].txn.appAccounts?.push(algosdk.decodeAddress(a.account))
+
+      // Remove resources from the group if we're adding them here
+      g.accounts = g.accounts?.filter((acc) => acc !== a.account)
+      g.assets = g.assets?.filter((asset) => BigInt(asset) !== BigInt(a.asset))
     })
 
     // Do accounts next because the account limit is 4
