8a32af54a7d689f47fecb5226eac7e95b4322951
Rob Moore (MakerX) <rob.moore@makerx.com.au>
2024-03-22 22:44:03 +0800

    Tweaks to AlgorandClient: (#248)
    * Tweaks to AlgorandClient:
    * Rename AlgoKitClient to AlgorandClient and added static construction methods
    * Added fluent with* methods to AlgorandClient to allow for fluent configuration
    * Extracted client related stuff to ClientManager and account stuff to AccountManager and added extra functionality to both
    * Add transaction and confirmation to the result of sending a single transaction for better ergonomics
    * Added ability to specify suggested params and create a copy of suggested params when providing it
    * Moved classes to types directory
    * Added getAccountInformation to make tests terser (this method should have been added ages ago)
    * Incorporated client into testing fixture
    * Changed all possible bigint | numbers' to number (where it's impractical for them to hit 2^53)
    * Incorporated TransactionSignerAccount with TransactionSigner to allow for terser code
    * Rename ID to Id for consistency with rest of algokit utils
    
    * use bigint where applicable
    
    ---------
    
    Co-authored-by: Joe Polny <joepolny@gmail.com>

---
+++ src/types/account-manager.ts
@@ -0,0 +1,239 @@
+import algosdk from 'algosdk'
+import * as algokit from '..'
+import { AccountInformation, SigningAccount, TransactionSignerAccount } from './account'
+import { AlgoAmount } from './amount'
+import { ClientManager } from './client-manager'
+import { SendTransactionFrom } from './transaction'
+
+/** Creates and keeps track of signing accounts against sending addresses. */
+export class AccountManager {
+  private _clientManager: ClientManager
+  private _accounts: { [address: string]: TransactionSignerAccount } = {}
+  private _defaultSigner?: algosdk.TransactionSigner
+
+  /**
+   * Create a new account creator
+   * @param algod The algod client to use
+   * @param kmd The optional kmd client to use
+   */
+  constructor(clientManager: ClientManager) {
+    this._clientManager = clientManager
+  }
+
+  /**
+   * Sets the default signer to use if no other signer is specified.
+   * @param signer The signer to use, either a `TransactionSigner` or a `TransactionSignerAccount`
+   * @returns The `AccountManager` so method calls can be chained
+   */
+  public withDefaultSigner(signer: algosdk.TransactionSigner | TransactionSignerAccount): AccountManager {
+    this._defaultSigner = 'signer' in signer ? signer.signer : signer
+    return this
+  }
+
+  /**
+   * Records the given account against the address of the account for later retrieval and returns a `TransactionSignerAccount`.
+   * @param account The account to use.
+   * @returns A `TransactionSignerAccount` for the given account.
+   */
+  private signerAccount<T extends SendTransactionFrom>(account: T): TransactionSignerAccount & { account: T } {
+    const acc = {
+      addr: algokit.getSenderAddress(account),
+      signer: algokit.getSenderTransactionSigner(account),
+    }
+    this._accounts[acc.addr] = acc
+    return { ...acc, account }
+  }
+
+  /**
+   * Tracks the given account for later signing.
+   * @param account The account to register
+   * @returns The AccountCreator instance for method chaining
+   */
+  public withAccount(account: TransactionSignerAccount | SendTransactionFrom) {
+    const acc =
+      'signer' in account && 'addr' in account
+        ? account
+        : { signer: algokit.getSenderTransactionSigner(account), addr: algokit.getSenderAddress(account) }
+    this._accounts[acc.addr] = acc
+    return this
+  }
+
+  /**
+   * Tracks the given account for later signing.
+   * @param sender The sender address to use this signer for
+   * @param signer The signer to sign transactions with for the given sender
+   * @returns The AccountCreator instance for method chaining
+   */
+  public withSigner(sender: string, signer: algosdk.TransactionSigner) {
+    this._accounts[sender] = { addr: sender, signer }
+    return this
+  }
+
+  /**
+   * Returns the `TransactionSigner` for the given sender address.
+   *
+   * If no signer has been registered for that address then the default signer is used if registered.
+   *
+   * @param sender The sender address
+   * @returns The `TransactionSigner` or throws an error if not found
+   */
+  public getSigner(sender: string): algosdk.TransactionSigner {
+    const signer = this._accounts[sender]?.signer ?? this._defaultSigner
+    if (!signer) throw new Error(`No signer found for address ${sender}`)
+    return signer
+  }
+
+  /**
+   * Returns the `TransactionSignerAccount` for the given sender address.
+   * @param sender The sender address
+   * @returns The `TransactionSignerAccount` or throws an error if not found
+   */
+  public getAccount(sender: string): TransactionSignerAccount {
+    const account = this._accounts[sender]
+    if (!account) throw new Error(`No signer found for address ${sender}`)
+    return account
+  }
+
+  /**
+   * Returns the given sender account's current status, balance and spendable amounts.
+   *
+   * @example
+   * ```typescript
+   * const address = "XBYLS2E6YI6XXL5BWCAMOA4GTWHXWENZMX5UHXMRNWWUQ7BXCY5WC5TEPA";
+   * const accountInfo = await account.getInformation(address);
+   * ```
+   *
+   * [Response data schema details](https://developer.algorand.org/docs/rest-apis/algod/#get-v2accountsaddress)
+   * @param sender The address of the sender/account to look up
+   * @returns The account information
+   */
+  public async getInformation(sender: string | TransactionSignerAccount): Promise<AccountInformation> {
+    return algokit.getAccountInformation(sender, this._clientManager.algod)
+  }
+
+  /**
+   * Tracks and returns an Algorand account with secret key loaded (i.e. that can sign transactions) by taking the mnemonic secret.
+   *
+   * @example
+   * ```typescript
+   * const account = await account.fromMnemonic("mnemonic secret ...")
+   * const rekeyedAccount = await account.fromMnemonic("mnemonic secret ...", "SENDERADDRESS...")
+   * ```
+   * @param mnemonicSecret The mnemonic secret representing the private key of an account; **Note: Be careful how the mnemonic is handled**,
+   *  never commit it into source control and ideally load it from the environment (ideally via a secret storage service) rather than the file system.
+   * @param sender The optional sender address to use this signer for (aka a rekeyed account)
+   * @returns The account
+   */
+  public fromMnemonic = (mnemonicSecret: string, sender?: string) => {
+    const account = algokit.mnemonicAccount(mnemonicSecret)
+    return this.signerAccount(sender ? algokit.rekeyedAccount(account, sender) : account)
+  }
+
+  /**
+   * Tracks and returns an Algorand account with private key loaded by convention from environment variables based on the given name identifier.
+   *
+   * Note: This function expects to run in a Node.js environment.
+   *
+   * ## Convention:
+   * * **Non-LocalNet:** will load process.env['\{NAME\}_MNEMONIC'] as a mnemonic secret; **Note: Be careful how the mnemonic is handled**,
+   *  never commit it into source control and ideally load it via a secret storage service rather than the file system.
+   *   If process.env['\{NAME\}_SENDER'] is defined then it will use that for the sender address (i.e. to support rekeyed accounts)
+   * * **LocalNet:** will load the account from a KMD wallet called \{NAME\} and if that wallet doesn't exist it will create it and fund the account for you
+   *
+   * This allows you to write code that will work seamlessly in production and local development (LocalNet) without manual config locally (including when you reset the LocalNet).
+   *
+   * @example Default
+   *
+   * If you have a mnemonic secret loaded into `process.env.MY_ACCOUNT_MNEMONIC` then you can call the following to get that private key loaded into an account object:
+   * ```typescript
+   * const account = await account.fromEnvironment('MY_ACCOUNT', algod)
+   * ```
+   *
+   * If that code runs against LocalNet then a wallet called `MY_ACCOUNT` will automatically be created with an account that is automatically funded with 1000 (default) ALGOs from the default LocalNet dispenser.
+   * If not running against LocalNet then it will use proces.env.MY_ACCOUNT_MNEMONIC as the private key and (if present) process.env.MY_ACCOUNT_SENDER as the sender address.
+   *
+   * @param name The name identifier of the account
+   * @param fundWith The optional amount to fund the account with when it gets created (when targeting LocalNet), if not specified then 1000 Algos will be funded from the dispenser account
+   * @returns The account
+   */
+  public fromEnvironment = async (name: string, fundWith?: AlgoAmount) =>
+    this.signerAccount(await algokit.mnemonicAccountFromEnvironment({ name, fundWith }, this._clientManager.algod, this._clientManager.kmd))
+
+  /**
+   * Tracks and returns an Algorand account with private key loaded from the given KMD wallet (identified by name).
+   *
+   * @param name: The name of the wallet to retrieve an account from
+   * @param predicate: An optional filter to use to find the account (otherwise it will return a random account from the wallet)
+   * @param sender The optional sender address to use this signer for (aka a rekeyed account)
+   * @example Get default funded account in a LocalNet
+   *
+   * ```typescript
+   * const defaultDispenserAccount = await account.fromKmd('unencrypted-default-wallet',
+   *   a => a.status !== 'Offline' && a.amount > 1_000_000_000
+   * )
+   * ```
+   * @returns The account
+   */
+  public fromKmd = async (
+    name: string,
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
+    predicate?: (account: Record<string, any>) => boolean,
+    sender?: string,
+  ) => {
+    const account = await algokit.getKmdWalletAccount({ name, predicate }, this._clientManager.algod, this._clientManager.kmd)
+    if (!account) throw new Error(`Unable to find KMD account ${name}${predicate ? ' with predicate' : ''}`)
+    return this.signerAccount(sender ? algokit.rekeyedAccount(account, sender) : account)
+  }
+
+  /**
+   * Tracks and returns an account that supports partial or full multisig signing.
+   *
+   * @example
+   * ```typescript
+   * const account = await account.multisig({version: 1, threshold: 1, addrs: ["ADDRESS1...", "ADDRESS2..."]},
+   *  await account.fromEnvironment('ACCOUNT1'))
+   * ```
+   * @param multisigParams The parameters that define the multisig account
+   * @param signingAccounts The signers that are currently present
+   * @returns A multisig account wrapper
+   */
+  public multisig = (multisigParams: algosdk.MultisigMetadata, signingAccounts: (algosdk.Account | SigningAccount)[]) => {
+    return this.signerAccount(algokit.multisigAccount(multisigParams, signingAccounts))
+  }
+
+  /**
+   * Tracks and returns a new, random Algorand account with secret key loaded.
+   *
+   * @example
+   * ```typescript
+   * const account = await account.random()
+   * ```
+   * @returns The account
+   */
+  public random = () => this.signerAccount(algokit.randomAccount())
+
+  /**
+   * Returns an account (with private key loaded) that can act as a dispenser.
+   *
+   * @example
+   * ```typescript
+   * const account = await account.dispenser()
+   * ```
+   * If running on LocalNet then it will return the default dispenser account automatically,
+   *  otherwise it will load the account mnemonic stored in process.env.DISPENSER_MNEMONIC.
+   * @returns The account
+   */
+  public dispenser = async () => this.signerAccount(await algokit.getDispenserAccount(this._clientManager.algod, this._clientManager.kmd))
+
+  /**
+   * Returns an Algorand account with private key loaded for the default LocalNet dispenser account (that can be used to fund other accounts).
+   *
+   * @example
+   * ```typescript
+   * const account = await account.localNetDispenser()
+   * ```
+   * @returns The account
+   */
+  public localNetDispenser = async () =>
+    this.signerAccount(await algokit.getLocalNetDispenserAccount(this._clientManager.algod, this._clientManager.kmd))
+}
---
+++ src/types/algorand-client.ts
@@ -0,0 +1,274 @@
+import algosdk from 'algosdk'
+import { getAlgoNodeConfig, getConfigFromEnvOrDefaults, getDefaultLocalNetConfig } from '..'
+import { TransactionSignerAccount } from './account'
+import { AccountManager } from './account-manager'
+import { AlgoSdkClients, ClientManager } from './client-manager'
+import AlgokitComposer, {
+  AppCallParams,
+  AssetConfigParams,
+  AssetCreateParams,
+  AssetDestroyParams,
+  AssetFreezeParams,
+  AssetOptInParams,
+  AssetTransferParams,
+  KeyRegParams,
+  MethodCallParams,
+  PayTxnParams,
+} from './composer'
+import { AlgoConfig } from './network-client'
+import { SendAtomicTransactionComposerResults, SendTransactionFrom } from './transaction'
+
+async function unwrapSingleSendResult(results: Promise<SendAtomicTransactionComposerResults>) {
+  const result = await results
+
+  return {
+    // Last item covers when a group is created by an app call with ABI transaction parameters
+    transaction: result.transactions[result.transactions.length - 1],
+    confirmation: result.confirmations![result.confirmations!.length - 1],
+    txId: result.txIds[0],
+    ...result,
+  }
+}
+
+/** A client that brokers easy access to Algorand functionality. */
+export default class AlgorandClient {
+  private _clientManager: ClientManager
+  private _accountManager: AccountManager
+
+  private _cachedSuggestedParams?: algosdk.SuggestedParams
+  private _cachedSuggestedParamsExpiry?: Date
+  private _cachedSuggestedParamsTimeout: number = 3_000 // three seconds
+
+  private constructor(config: AlgoConfig | AlgoSdkClients) {
+    this._clientManager = new ClientManager(config)
+    this._accountManager = new AccountManager(this._clientManager)
+  }
+
+  /**
+   * Sets the default signer to use if no other signer is specified.
+   * @param signer The signer to use, either a `TransactionSigner` or a `TransactionSignerAccount`
+   * @returns The `AlgorandClient` so method calls can be chained
+   */
+  public withDefaultSigner(signer: algosdk.TransactionSigner | TransactionSignerAccount): AlgorandClient {
+    this._accountManager.withDefaultSigner(signer)
+    return this
+  }
+
+  /**
+   * Tracks the given account for later signing.
+   * @param account The account to register
+   * @returns The `AlgorandClient` so method calls can be chained
+   */
+  public withAccount(account: TransactionSignerAccount | SendTransactionFrom) {
+    this._accountManager.withAccount(account)
+    return this
+  }
+
+  /**
+   * Tracks the given account for later signing.
+   * @param sender The sender address to use this signer for
+   * @param signer The signer to sign transactions with for the given sender
+   * @returns The `AlgorandClient` so method calls can be chained
+   */
+  public withSigner(sender: string, signer: algosdk.TransactionSigner) {
+    this._accountManager.withSigner(sender, signer)
+    return this
+  }
+
+  /**
+   * Sets a cache value to use for suggested params.
+   * @param suggestedParams The suggested params to use
+   * @param until A date until which to cache, or if not specified then the timeout is used
+   * @returns The `AlgorandClient` so method calls can be chained
+   */
+  public withSuggestedParams(suggestedParams: algosdk.SuggestedParams, until?: Date) {
+    this._cachedSuggestedParams = suggestedParams
+    this._cachedSuggestedParamsExpiry = until ?? new Date(+new Date() + this._cachedSuggestedParamsTimeout)
+    return this
+  }
+
+  /**
+   * Sets the timeout for caching suggested params.
+   * @param timeout The timeout in milliseconds
+   * @returns The `AlgorandClient` so method calls can be chained
+   */
+  public withSuggestedParamsTimeout(timeout: number) {
+    this._cachedSuggestedParamsTimeout = timeout
+    return this
+  }
+
+  /** Get suggested params for a transaction (either cached or from algod if the cache is stale or empty) */
+  async getSuggestedParams(): Promise<algosdk.SuggestedParams> {
+    if (this._cachedSuggestedParams && (!this._cachedSuggestedParamsExpiry || this._cachedSuggestedParamsExpiry > new Date())) {
+      return {
+        ...this._cachedSuggestedParams,
+      }
+    }
+
+    this._cachedSuggestedParams = await this._clientManager.algod.getTransactionParams().do()
+    this._cachedSuggestedParamsExpiry = new Date(new Date().getTime() + this._cachedSuggestedParamsTimeout)
+
+    return {
+      ...this._cachedSuggestedParams,
+    }
+  }
+
+  /** Get clients, including algosdk clients and app clients. */
+  public get client() {
+    return this._clientManager
+  }
+
+  /** Get or create accounts that can sign transactions. */
+  public get account() {
+    return this._accountManager
+  }
+
+  /** Start a new `AlgokitComposer` transaction group */
+  newGroup() {
+    return new AlgokitComposer(
+      this.client.algod,
+      (addr: string) => this.account.getSigner(addr),
+      () => this.getSuggestedParams(),
+    )
+  }
+
+  /**
+   * Methods for sending a transaction
+   */
+  send = {
+    payment: (params: PayTxnParams) => {
+      return unwrapSingleSendResult(this.newGroup().addPayment(params).execute())
+    },
+    assetCreate: (params: AssetCreateParams) => {
+      return unwrapSingleSendResult(this.newGroup().addAssetCreate(params).execute())
+    },
+    assetConfig: (params: AssetConfigParams) => {
+      return unwrapSingleSendResult(this.newGroup().addAssetConfig(params).execute())
+    },
+    assetFreeze: (params: AssetFreezeParams) => {
+      return unwrapSingleSendResult(this.newGroup().addAssetFreeze(params).execute())
+    },
+    assetDestroy: (params: AssetDestroyParams) => {
+      return unwrapSingleSendResult(this.newGroup().addAssetDestroy(params).execute())
+    },
+    assetTransfer: (params: AssetTransferParams) => {
+      return unwrapSingleSendResult(this.newGroup().addAssetTransfer(params).execute())
+    },
+    appCall: (params: AppCallParams) => {
+      return unwrapSingleSendResult(this.newGroup().addAppCall(params).execute())
+    },
+    keyReg: (params: KeyRegParams) => {
+      return unwrapSingleSendResult(this.newGroup().addKeyReg(params).execute())
+    },
+    methodCall: (params: MethodCallParams) => {
+      return unwrapSingleSendResult(this.newGroup().addMethodCall(params).execute())
+    },
+    assetOptIn: (params: AssetOptInParams) => {
+      return unwrapSingleSendResult(this.newGroup().addAssetOptIn(params).execute())
+    },
+  }
+
+  /**
+   * Methods for building transactions
+   */
+  transactions = {
+    payment: async (params: PayTxnParams) => {
+      return (await this.newGroup().addPayment(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+    assetCreate: async (params: AssetCreateParams) => {
+      return (await this.newGroup().addAssetCreate(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+    assetConfig: async (params: AssetConfigParams) => {
+      return (await this.newGroup().addAssetConfig(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+    assetFreeze: async (params: AssetFreezeParams) => {
+      return (await this.newGroup().addAssetFreeze(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+    assetDestroy: async (params: AssetDestroyParams) => {
+      return (await this.newGroup().addAssetDestroy(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+    assetTransfer: async (params: AssetTransferParams) => {
+      return (await this.newGroup().addAssetTransfer(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+    appCall: async (params: AppCallParams) => {
+      return (await this.newGroup().addAppCall(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+    keyReg: async (params: KeyRegParams) => {
+      return (await this.newGroup().addKeyReg(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+    methodCall: async (params: MethodCallParams) => {
+      return (await this.newGroup().addMethodCall(params).buildGroup()).map((ts) => ts.txn)
+    },
+    assetOptIn: async (params: AssetOptInParams) => {
+      return (await this.newGroup().addAssetOptIn(params).buildGroup()).map((ts) => ts.txn)[0]
+    },
+  }
+
+  // Static methods to create an `AlgorandClient`
+
+  /**
+   * Returns an `AlgorandClient` pointing at default LocalNet ports and API token.
+   * @returns The `AlgorandClient`
+   */
+  public static defaultLocalNet() {
+    return new AlgorandClient({
+      algodConfig: getDefaultLocalNetConfig('algod'),
+      indexerConfig: getDefaultLocalNetConfig('indexer'),
+      kmdConfig: getDefaultLocalNetConfig('kmd'),
+    })
+  }
+
+  /**
+   * Returns an `AlgorandClient` pointing at TestNet using AlgoNode.
+   * @returns The `AlgorandClient`
+   */
+  public static testNet() {
+    return new AlgorandClient({
+      algodConfig: getAlgoNodeConfig('testnet', 'algod'),
+      indexerConfig: getAlgoNodeConfig('testnet', 'indexer'),
+      kmdConfig: undefined,
+    })
+  }
+
+  /**
+   * Returns an `AlgorandClient` pointing at MainNet using AlgoNode.
+   * @returns The `AlgorandClient`
+   */
+  public static mainNet() {
+    return new AlgorandClient({
+      algodConfig: getAlgoNodeConfig('mainnet', 'algod'),
+      indexerConfig: getAlgoNodeConfig('mainnet', 'indexer'),
+      kmdConfig: undefined,
+    })
+  }
+
+  /**
+   * Returns an `AlgorandClient` pointing to the given client(s).
+   * @param clients The clients to use
+   * @returns The `AlgorandClient`
+   */
+  public static fromClients(clients: AlgoSdkClients) {
+    return new AlgorandClient(clients)
+  }
+
+  /**
+   * Returns an `AlgorandClient` loading the configuration from environment variables.
+   *
+   * Retrieve configurations from environment variables when defined or get defaults.
+   *
+   * Expects to be called from a Node.js environment.
+   * @returns The `AlgorandClient`
+   */
+  public static fromEnvironment() {
+    return new AlgorandClient(getConfigFromEnvOrDefaults())
+  }
+
+  /**
+   * Returns an `AlgorandClient` from the given config.
+   * @param config The config to use
+   * @returns The `AlgorandClient`
+   */
+  public static fromConfig(config: AlgoConfig) {
+    return new AlgorandClient(config)
+  }
+}
