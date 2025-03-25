5e80e8595d2f9611ba3bf76cc3dc53ca9f2cd2e7
Larkin Young <larkin.young@algorand.foundation>
2025-03-25 14:40:36 +0000

    Links

--- src/indexer-lookup.ts
+++ src/indexer-lookup.ts
@@ -140,7 +140,7 @@ export async function searchTransactions(
   })
 }
 
-// https://dev.algorand.co/reference/rest-api/indexer
+// https://dev.algorand.co/reference/rest-apis/indexer
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export async function executePaginatedRequest<TResult, TRequest extends { do: () => Promise<any> }>(
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
