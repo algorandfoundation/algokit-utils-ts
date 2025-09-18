3f6856a17f4ababb2b4841d811dd6df62d2eb63b
Hoang Dinh <patrick.dinh@makerx.com.au>
2025-09-18 14:30:08 +1000

    comments

--- src/core/utils/transactions/sender.ts
+++ src/core/utils/transactions/sender.ts
@@ -131,10 +131,26 @@ export class TransactionSender {
     }
   }
 
+  /**
+   * Send a payment transaction to transfer Algo between accounts.
+   * @param params The parameters for the payment transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the payment transaction and the transaction that was sent
+   */
   public async payment(params: PaymentParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addPayment(p), sendParams)
   }
 
+  /**
+   * Create a new Algorand Standard Asset.
+   *
+   * The account that sends this transaction will automatically be
+   * opted in to the asset and will hold all units after creation.
+   *
+   * @param params The parameters for the asset creation transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset create transaction and the transaction that was sent
+   */
   public async assetCreate(params: AssetCreateParams, sendParams?: SendParams): Promise<SendAssetCreateResult> {
     return this.sendSingleTransactionWithResult(
       params,
@@ -153,26 +169,80 @@ export class TransactionSender {
     )
   }
 
+  /**
+   * Configure an existing Algorand Standard Asset.
+   *
+   * **Note:** The manager, reserve, freeze, and clawback addresses
+   * are immutably empty if they are not set. If manager is not set then
+   * all fields are immutable from that point forward.
+   *
+   * @param params The parameters for the asset config transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset config transaction and the transaction that was sent
+   */
   public async assetConfig(params: AssetConfigParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAssetConfig(p), sendParams)
   }
 
+  /**
+   * Freeze an Algorand Standard Asset for an account.
+   *
+   * @param params The parameters for the asset freeze transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset freeze transaction and the transaction that was sent
+   */
   public async assetFreeze(params: AssetFreezeParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAssetFreeze(p), sendParams)
   }
 
+  /**
+   * Destroys an Algorand Standard Asset.
+   *
+   * Created assets can be destroyed only by the asset manager account.
+   * All of the assets must be owned by the creator of the asset before
+   * the asset can be deleted.
+   *
+   * @param params The parameters for the asset destroy transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset destroy transaction and the transaction that was sent
+   */
   public async assetDestroy(params: AssetDestroyParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAssetDestroy(p), sendParams)
   }
 
+  /**
+   * Transfer an Algorand Standard Asset.
+   *
+   * @param params The parameters for the asset transfer transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset transfer transaction and the transaction that was sent
+   */
   public async assetTransfer(params: AssetTransferParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAssetTransfer(p), sendParams)
   }
 
+  /**
+   * Opt an account into an Algorand Standard Asset.
+   *
+   * @param params The parameters for the asset opt-in transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset opt-in transaction and the transaction that was sent
+   */
   public async assetOptIn(params: AssetOptInParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAssetOptIn(p), sendParams)
   }
 
+  /**
+   * Opt an account out of an Algorand Standard Asset.
+   *
+   * *Note:* If the account has a balance of the asset,
+   * it will not be able to opt-out unless `ensureZeroBalance`
+   * is set to `false` (but then the account will lose the assets).
+   *
+   * @param params The parameters for the asset opt-out transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset opt-out transaction and the transaction that was sent
+   */
   public async assetOptOut(params: AssetOptOutParams & { ensureZeroBalance?: boolean }, sendParams?: SendParams): Promise<SendResult> {
     const shouldCheckBalance = params.ensureZeroBalance ?? false
 
@@ -200,22 +270,58 @@ export class TransactionSender {
     return this.sendSingleTransaction(updatedParams, (composer, p) => composer.addAssetOptOut(p), sendParams)
   }
 
+  /**
+   * Close an account and transfer remaining balance to another account.
+   *
+   * *Warning:* Be careful this can lead to loss of funds if not used correctly.
+   * @param params The parameters for the account close transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the account close transaction and the transaction that was sent
+   */
   public async accountClose(params: AccountCloseParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAccountClose(p), sendParams)
   }
 
+  /**
+   * Clawback an Algorand Standard Asset from an account.
+   *
+   * @param params The parameters for the asset clawback transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset clawback transaction and the transaction that was sent
+   */
   public async assetClawback(params: AssetClawbackParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAssetClawback(p), sendParams)
   }
 
+  /**
+   * Unfreeze an Algorand Standard Asset for an account.
+   *
+   * @param params The parameters for the asset unfreeze transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the asset unfreeze transaction and the transaction that was sent
+   */
   public async assetUnfreeze(params: AssetUnfreezeParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAssetUnfreeze(p), sendParams)
   }
 
+  /**
+   * Call a smart contract.
+   *
+   * @param params The parameters for the app call transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the app call transaction and the transaction that was sent
+   */
   async appCall(params: AppCallParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAppCall(p), sendParams)
   }
 
+  /**
+   * Create a smart contract.
+   *
+   * @param params The parameters for the app creation transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the app create transaction and the transaction that was sent
+   */
   async appCreate(params: AppCreateParams, sendParams?: SendParams): Promise<SendAppCreateResult> {
     return this.sendSingleTransactionWithResult(
       params,
@@ -234,18 +340,46 @@ export class TransactionSender {
     )
   }
 
+  /**
+   * Update a smart contract.
+   *
+   * @param params The parameters for the app update transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the app update transaction and the transaction that was sent
+   */
   async appUpdate(params: AppUpdateParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAppUpdate(p), sendParams)
   }
 
+  /**
+   * Delete a smart contract.
+   *
+   * @param params The parameters for the app deletion transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the app delete transaction and the transaction that was sent
+   */
   async appDelete(params: AppDeleteParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addAppDelete(p), sendParams)
   }
 
+  /**
+   * Call a smart contract via an ABI method.
+   *
+   * @param params The parameters for the app call transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the application ABI method call transaction and the transaction that was sent
+   */
   async appCallMethodCall(params: AppCallMethodCallParams, sendParams?: SendParams): Promise<SendAppCallMethodCallResult> {
     return this.sendMethodCall(params, (composer, p) => composer.addAppCallMethodCall(p), sendParams)
   }
 
+  /**
+   * Create a smart contract via an ABI method.
+   *
+   * @param params The parameters for the app creation transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the application ABI method create transaction and the transaction that was sent
+   */
   async appCreateMethodCall(params: AppCreateMethodCallParams, sendParams?: SendParams): Promise<SendAppCreateMethodCallResult> {
     return this.sendMethodCallWithResult(
       params,
@@ -264,22 +398,57 @@ export class TransactionSender {
     )
   }
 
+  /**
+   * Update a smart contract via an ABI method.
+   *
+   * @param params The parameters for the app update transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the application ABI method update transaction and the transaction that was sent
+   */
   async appUpdateMethodCall(params: AppUpdateMethodCallParams, sendParams?: SendParams): Promise<SendAppCallMethodCallResult> {
     return this.sendMethodCall(params, (composer, p) => composer.addAppUpdateMethodCall(p), sendParams)
   }
 
+  /**
+   * Delete a smart contract via an ABI method.
+   *
+   * @param params The parameters for the app deletion transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the application ABI method delete transaction and the transaction that was sent
+   */
   async appDeleteMethodCall(params: AppDeleteMethodCallParams, sendParams?: SendParams): Promise<SendAppCallMethodCallResult> {
     return this.sendMethodCall(params, (composer, p) => composer.addAppDeleteMethodCall(p), sendParams)
   }
 
+  /**
+   * Register an online key.
+   *
+   * @param params The parameters for the key registration transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the online key registration transaction and the transaction that was sent
+   */
   async onlineKeyRegistration(params: OnlineKeyRegistrationParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addOnlineKeyRegistration(p), sendParams)
   }
 
+  /**
+   * Register an offline key.
+   *
+   * @param params The parameters for the key registration transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the offline key registration transaction and the transaction that was sent
+   */
   async offlineKeyRegistration(params: OfflineKeyRegistrationParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addOfflineKeyRegistration(p), sendParams)
   }
 
+  /**
+   * Register a non-participation key.
+   *
+   * @param params The parameters for the key registration transaction
+   * @param sendParams Optional parameters for sending the transaction
+   * @returns The result of the non-participation key registration transaction and the transaction that was sent
+   */
   async nonParticipationKeyRegistration(params: NonParticipationKeyRegistrationParams, sendParams?: SendParams): Promise<SendResult> {
     return this.sendSingleTransaction(params, (composer, p) => composer.addNonParticipationKeyRegistration(p), sendParams)
   }
