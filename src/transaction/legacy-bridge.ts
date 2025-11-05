89dda1e90c192b693eda72cbcfc4199064665008
Hoang Dinh <patrick.dinh@makerx.com.au>
2025-11-05 15:28:13 +1000

    remove comment

--- src/transaction/legacy-bridge.ts
+++ src/transaction/legacy-bridge.ts
@@ -150,8 +150,7 @@ export async function _getAppArgsForABICall(args: ABIAppCallArgs, from: SendTran
           ? { txn: (await a).transaction, signer }
           : 'transaction' in a
             ? { txn: a.transaction, signer: 'signer' in a ? getSenderTransactionSigner(a.signer) : signer }
-            : // TODO: PD - revert this after the transaction class is done
-              'type' in a
+            : 'type' in a
               ? { txn: a, signer }
               : a
     }),
