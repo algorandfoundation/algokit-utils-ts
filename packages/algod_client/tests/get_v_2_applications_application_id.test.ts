978de1a4ff547a2141a35df99914b8096a540f23
David Rojas <lempira@gmail.com>
2025-11-14 17:17:47 -0500

    feat(tests): add application endpoint test with external snapshots
      - Add get_v_2_applications_application_id test

--- packages/algod_client/tests/get_v_2_applications_application_id.test.ts
+++ packages/algod_client/tests/get_v_2_applications_application_id.test.ts
@@ -1,15 +1,19 @@
-import { expect, test, describe } from "vitest";
+import { describe, expect, test } from 'vitest'
+import { AlgodClient } from '../src/client'
+import { config, TEST_APP_ID } from './config'
 
-describe("GET v2_applications_APPLICATION-ID", () => {
+describe('GET v2_applications_APPLICATION-ID', () => {
   // Polytest Suite: GET v2_applications_APPLICATION-ID
 
-  describe("Common Tests", () => {
+  describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test("Basic request and response validation", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('Basic request and response validation', async () => {
+      const client = new AlgodClient(config)
 
-  });
+      const result = await client.getApplicationById(TEST_APP_ID)
 
-});
+      expect(result).toMatchSnapshot()
+    })
+  })
+})
