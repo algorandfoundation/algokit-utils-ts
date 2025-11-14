3923f67996bb9829160f9794604d9f2af4071dec
David Rojas <lempira@gmail.com>
2025-11-14 17:23:25 -0500

    feat(tests): add ledger endpoint tests with external snapshots
      - Add get_v_2_ledger_supply test
      - Add get_v_2_ledger_sync test

--- packages/algod_client/tests/get_v_2_ledger_supply.test.ts
+++ packages/algod_client/tests/get_v_2_ledger_supply.test.ts
@@ -1,15 +1,19 @@
-import { expect, test, describe } from "vitest";
+import { describe, expect, test } from 'vitest'
+import { AlgodClient } from '../src/client'
+import { config } from './config'
 
-describe("GET v2_ledger_supply", () => {
+describe('GET v2_ledger_supply', () => {
   // Polytest Suite: GET v2_ledger_supply
 
-  describe("Common Tests", () => {
+  describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test("Basic request and response validation", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('Basic request and response validation', async () => {
+      const client = new AlgodClient(config)
 
-  });
+      const result = await client.getSupply()
 
-});
+      expect(result).toMatchSnapshot()
+    })
+  })
+})
