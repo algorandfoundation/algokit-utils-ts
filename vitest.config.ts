1fc589263dc0458bae773670937fca47dc369afe
Neil Campbell <neil.campbell@makerx.com.au>
2025-11-22 00:33:56 +0800

    fix: prevent tests inside packages from running twice

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
   test: {
     projects: ['.', 'packages/*'],
     include: ['**/*.spec.ts', '**/*.test.ts'],
-    exclude: ['node_modules'],
+    exclude: ['node_modules', 'packages/**'],
     // Sometimes indexer catchup is slowwwww...
     testTimeout: 20_000,
     coverage: {
