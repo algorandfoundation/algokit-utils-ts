a511539859f13d414cd0a1794616245c8e44a4e7
David Rojas <lempira@gmail.com>
2025-11-22 15:56:58 -0500

    test(indexer): add transaction and block endpoint tests - Search for transactions - Lookup transaction by TXID - Lookup block by round number

--- packages/indexer_client/tests/get_v_2_block_headers.test.ts
+++ packages/indexer_client/tests/get_v_2_block_headers.test.ts
@@ -1,15 +1,25 @@
-import { expect, test, describe } from "vitest";
+import { describe, expect, expectTypeOf, test } from 'vitest'
+import { IndexerClient } from '../src/client'
+import type { SearchForBlockHeaders } from '../src/models/search-for-block-headers'
+import { SearchForBlockHeadersMeta } from '../src/models/search-for-block-headers'
+import { config } from './config'
+import { modelMetadataToZodSchema } from './zod-utils'
 
-describe("GET v2_block-headers", () => {
+describe('GET v2_block-headers', () => {
   // Polytest Suite: GET v2_block-headers
 
-  describe("Common Tests", () => {
+  describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test("Basic request and response validation", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('Basic request and response validation', async () => {
+      const client = new IndexerClient(config)
 
-  });
+      const result = await client.searchForBlockHeaders({ limit: 1 })
 
-});
+      // Assert response structure
+      expectTypeOf(result).toEqualTypeOf<SearchForBlockHeaders>()
+      const SearchForBlockHeadersSchema = modelMetadataToZodSchema(SearchForBlockHeadersMeta)
+      expect(() => SearchForBlockHeadersSchema.parse(result)).not.toThrow()
+    })
+  })
+})
