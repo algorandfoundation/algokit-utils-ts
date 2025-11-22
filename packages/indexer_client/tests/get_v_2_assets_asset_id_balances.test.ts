be9475a67905be1c00e1570599d49e9241e74896
David Rojas <lempira@gmail.com>
2025-11-22 15:37:24 -0500

    test(indexer): add asset endpoints tests
    - Search for assets
    - Lookup asset by ID
    - Lookup asset balances
    - Lookup asset transactions

--- packages/indexer_client/tests/get_v_2_assets_asset_id_balances.test.ts
+++ packages/indexer_client/tests/get_v_2_assets_asset_id_balances.test.ts
@@ -1,15 +1,25 @@
-import { expect, test, describe } from "vitest";
+import { describe, expect, expectTypeOf, test } from 'vitest'
+import { IndexerClient } from '../src/client'
+import type { LookupAssetBalances } from '../src/models/lookup-asset-balances'
+import { LookupAssetBalancesMeta } from '../src/models/lookup-asset-balances'
+import { config, TEST_ASSET_ID } from './config'
+import { modelMetadataToZodSchema } from './zod-utils'
 
-describe("GET v2_assets_ASSET-ID_balances", () => {
+describe('GET v2_assets_ASSET-ID_balances', () => {
   // Polytest Suite: GET v2_assets_ASSET-ID_balances
 
-  describe("Common Tests", () => {
+  describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test("Basic request and response validation", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('Basic request and response validation', async () => {
+      const client = new IndexerClient(config)
 
-  });
+      const result = await client.lookupAssetBalances(TEST_ASSET_ID)
 
-});
+      // Assert response structure
+      expectTypeOf(result).toEqualTypeOf<LookupAssetBalances>()
+      const LookupAssetBalancesSchema = modelMetadataToZodSchema(LookupAssetBalancesMeta)
+      expect(() => LookupAssetBalancesSchema.parse(result)).not.toThrow()
+    })
+  })
+})
--- packages/indexer_client/tests/get_v_2_assets_asset_id_transactions.test.ts
+++ packages/indexer_client/tests/get_v_2_assets_asset_id_transactions.test.ts
@@ -1,15 +1,25 @@
-import { expect, test, describe } from "vitest";
+import { describe, expect, expectTypeOf, test } from 'vitest'
+import { IndexerClient } from '../src/client'
+import type { LookupAssetTransactions } from '../src/models/lookup-asset-transactions'
+import { LookupAssetTransactionsMeta } from '../src/models/lookup-asset-transactions'
+import { config, TEST_ASSET_ID } from './config'
+import { modelMetadataToZodSchema } from './zod-utils'
 
-describe("GET v2_assets_ASSET-ID_transactions", () => {
+describe('GET v2_assets_ASSET-ID_transactions', () => {
   // Polytest Suite: GET v2_assets_ASSET-ID_transactions
 
-  describe("Common Tests", () => {
+  describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test("Basic request and response validation", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('Basic request and response validation', async () => {
+      const client = new IndexerClient(config)
 
-  });
+      const result = await client.lookupAssetTransactions(TEST_ASSET_ID)
 
-});
+      // Assert response structure
+      expectTypeOf(result).toEqualTypeOf<LookupAssetTransactions>()
+      const LookupAssetTransactionsSchema = modelMetadataToZodSchema(LookupAssetTransactionsMeta)
+      expect(() => LookupAssetTransactionsSchema.parse(result)).not.toThrow()
+    })
+  })
+})
