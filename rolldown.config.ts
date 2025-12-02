4349cd8fb5a12c1adf32943316082f8d2341d30d
Hoang Dinh <patrick.dinh@makerx.com.au>
2025-12-02 15:20:34 +1000

    chore: re-export packages in utils

--- rolldown.config.ts
+++ rolldown.config.ts
@@ -3,5 +3,17 @@ import createConfig from './rolldown'
 
 export default createConfig(
   [...Object.keys(pkg.dependencies || {})],
-  ['src/index.ts', 'src/testing/index.ts', 'src/types/*.ts', '!src/types/*.spec.ts'],
+  [
+    'src/index.ts',
+    'src/testing/index.ts',
+    'src/types/*.ts',
+    '!src/types/*.spec.ts',
+    // Package re-exports
+    'src/abi/index.ts',
+    'src/common/index.ts',
+    'src/transact/index.ts',
+    'src/algod-client/index.ts',
+    'src/indexer-client/index.ts',
+    'src/kmd-client/index.ts',
+  ],
 )
