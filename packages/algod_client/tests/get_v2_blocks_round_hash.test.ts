d0a3f5303c1cf48c2ec3eb15435ac7e4d33869a7
David Rojas <lempira@gmail.com>
2025-12-16 14:41:19 -0500

    test(algod_client): implement asset and block endpoint tests

--- packages/algod_client/tests/get_v2_blocks_round_hash.test.ts
+++ packages/algod_client/tests/get_v2_blocks_round_hash.test.ts
@@ -1,4 +1,7 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+import { AlgodClient } from '../src'
+import { config, TEST_ROUND } from './config'
+import { BlockHashResponse } from './schemas'
 
 describe('GET v2_blocks_ROUND_hash', () => {
   // Polytest Suite: GET v2_blocks_ROUND_hash
@@ -6,8 +9,13 @@ describe('GET v2_blocks_ROUND_hash', () => {
   describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test('Basic request and response validation', () => {
-      throw new Error('TEST NOT IMPLEMENTED')
+    test('Basic request and response validation', async () => {
+      const client = new AlgodClient(config)
+
+      const result = await client.getBlockHash(TEST_ROUND)
+
+      BlockHashResponse.parse(result)
+      expect(result).toMatchSnapshot()
     })
   })
 })
--- packages/algod_client/tests/get_v2_blocks_round_lightheader_proof.test.ts
+++ packages/algod_client/tests/get_v2_blocks_round_lightheader_proof.test.ts
@@ -1,4 +1,7 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+import { AlgodClient } from '../src'
+import { config, TEST_ROUND } from './config'
+import { LightBlockHeaderProof } from './schemas'
 
 describe('GET v2_blocks_ROUND_lightheader_proof', () => {
   // Polytest Suite: GET v2_blocks_ROUND_lightheader_proof
@@ -6,8 +9,13 @@ describe('GET v2_blocks_ROUND_lightheader_proof', () => {
   describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test('Basic request and response validation', () => {
-      throw new Error('TEST NOT IMPLEMENTED')
+    test('Basic request and response validation', async () => {
+      const client = new AlgodClient(config)
+
+      const result = await client.getLightBlockHeaderProof(TEST_ROUND)
+
+      LightBlockHeaderProof.parse(result)
+      expect(result).toMatchSnapshot()
     })
   })
 })
