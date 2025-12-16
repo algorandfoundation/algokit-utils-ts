3a4e2b5a6632b172dcd79dcb1cecda109665384f
David Rojas <lempira@gmail.com>
2025-12-16 14:33:04 -0500

    test(algod_client): implement application endpoint tests

--- packages/algod_client/tests/get_v2_applications_application_id.test.ts
+++ packages/algod_client/tests/get_v2_applications_application_id.test.ts
@@ -1,4 +1,7 @@
-import { describe, test } from 'vitest'
+import { describe, expect, test } from 'vitest'
+import { AlgodClient } from '../src'
+import { config, TEST_APP_ID } from './config'
+import { Application } from './schemas'
 
 describe('GET v2_applications_APPLICATION-ID', () => {
   // Polytest Suite: GET v2_applications_APPLICATION-ID
@@ -6,8 +9,13 @@ describe('GET v2_applications_APPLICATION-ID', () => {
   describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test('Basic request and response validation', () => {
-      throw new Error('TEST NOT IMPLEMENTED')
+    test('Basic request and response validation', async () => {
+      const client = new AlgodClient(config)
+
+      const result = await client.getApplicationById(TEST_APP_ID)
+
+      Application.parse(result)
+      expect(result).toMatchSnapshot()
     })
   })
 })
