ca59c87987fe8b110db07dd2e69896ff2fe3612b
David Rojas <lempira@gmail.com>
2025-12-16 20:21:08 -0500

    test(kmd_client): implement GET versions and wallets tests

--- packages/kmd_client/tests/get_v1_wallets.test.ts
+++ packages/kmd_client/tests/get_v1_wallets.test.ts
@@ -1,15 +1,20 @@
-import { expect, test, describe } from "vitest";
+import { describe, test } from 'vitest'
+import { KmdClient } from '../src/client'
+import { localnetConfig } from './config'
+import { ListWalletsResponse } from './schemas'
 
-describe("GET v1_wallets", () => {
+describe('GET v1_wallets', () => {
   // Polytest Suite: GET v1_wallets
 
-  describe("Common Tests", () => {
+  describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test("Basic request and response validation", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('Basic request and response validation', async () => {
+      const client = new KmdClient(localnetConfig)
 
-  });
+      const result = await client.listWallets()
 
-});
+      ListWalletsResponse.parse(result)
+    })
+  })
+})
--- packages/kmd_client/tests/get_versions.test.ts
+++ packages/kmd_client/tests/get_versions.test.ts
@@ -1,15 +1,20 @@
-import { expect, test, describe } from "vitest";
+import { describe, test } from 'vitest'
+import { KmdClient } from '../src/client'
+import { localnetConfig } from './config'
+import { VersionsResponse } from './schemas'
 
-describe("GET versions", () => {
+describe('GET versions', () => {
   // Polytest Suite: GET versions
 
-  describe("Common Tests", () => {
+  describe('Common Tests', () => {
     // Polytest Group: Common Tests
 
-    test("Basic request and response validation", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('Basic request and response validation', async () => {
+      const client = new KmdClient(localnetConfig)
 
-  });
+      const result = await client.getVersion()
 
-});
+      VersionsResponse.parse(result)
+    })
+  })
+})
