d024eff4bb4b3b036f46095448d6d0d7a48c74fe
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 16:54:44 +1000

    chore: add deprecated re-exports for app-spec in types

---
+++ src/types/app-spec.ts
@@ -0,0 +1,81 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/app-spec'
+
+import {
+  arc32ToArc56 as _arc32ToArc56,
+  type AppSpec as _AppSpec,
+  type HintSpec as _HintSpec,
+  type AppSources as _AppSources,
+  type CallConfigValue as _CallConfigValue,
+  type CallConfig as _CallConfig,
+  type Hint as _Hint,
+  type FieldName as _FieldName,
+  type ABIType as _ABIType,
+  type StructElement as _StructElement,
+  type Struct as _Struct,
+  type DefaultArgument as _DefaultArgument,
+  type AVMType as _AVMType,
+  type DeclaredSchemaValueSpec as _DeclaredSchemaValueSpec,
+  type ReservedSchemaValueSpec as _ReservedSchemaValueSpec,
+  type SchemaSpec as _SchemaSpec,
+  type Schema as _Schema,
+  type StateSchemaSpec as _StateSchemaSpec,
+  type StateSchema as _StateSchema,
+} from '../app-spec'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export const arc32ToArc56 = _arc32ToArc56
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type AppSpec = _AppSpec
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type HintSpec = _HintSpec
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type AppSources = _AppSources
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type CallConfigValue = _CallConfigValue
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type CallConfig = _CallConfig
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type Hint = _Hint
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type FieldName = _FieldName
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type ABIType = _ABIType
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type StructElement = _StructElement
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type Struct = _Struct
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type DefaultArgument = _DefaultArgument
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type AVMType = _AVMType
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type DeclaredSchemaValueSpec = _DeclaredSchemaValueSpec
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type ReservedSchemaValueSpec = _ReservedSchemaValueSpec
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type SchemaSpec = _SchemaSpec
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type Schema = _Schema
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type StateSchemaSpec = _StateSchemaSpec
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-spec` instead */
+export type StateSchema = _StateSchema
