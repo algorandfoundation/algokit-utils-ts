14856d875a4b51382dc14fd876bcfb5358ee0217
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 16:21:01 +1000

    chore: add deprecated re-exports for algorand-client-transaction-creator in types

---
+++ src/types/algorand-client-transaction-creator.ts
@@ -0,0 +1,9 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/algorand-client-transaction-creator'
+
+import { AlgorandClientTransactionCreator as _AlgorandClientTransactionCreator } from '../algorand-client-transaction-creator'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/algorand-client-transaction-creator` instead */
+export const AlgorandClientTransactionCreator = _AlgorandClientTransactionCreator
+/** @deprecated Import from `@algorandfoundation/algokit-utils/algorand-client-transaction-creator` instead */
+export type AlgorandClientTransactionCreator = _AlgorandClientTransactionCreator
