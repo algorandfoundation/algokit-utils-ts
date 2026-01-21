bed12e31be0f44e44aec52397ec6e3efc3cb12f2
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 17:05:38 +1000

    chore: add deprecated re-exports for composer in types

---
+++ src/types/composer.ts
@@ -0,0 +1,143 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/composer'
+
+import {
+  type AppCallParams as _AppCallParams,
+  type AppCreateParams as _AppCreateParams,
+  type AppDeleteParams as _AppDeleteParams,
+  type AppMethodCallParams as _AppMethodCallParams,
+  type AppUpdateParams as _AppUpdateParams,
+  type CommonAppCallParams as _CommonAppCallParams,
+  type AssetConfigParams as _AssetConfigParams,
+  type AssetCreateParams as _AssetCreateParams,
+  type AssetDestroyParams as _AssetDestroyParams,
+  type AssetFreezeParams as _AssetFreezeParams,
+  type AssetOptInParams as _AssetOptInParams,
+  type AssetOptOutParams as _AssetOptOutParams,
+  type AssetTransferParams as _AssetTransferParams,
+  type CommonTransactionParams as _CommonTransactionParams,
+  type OfflineKeyRegistrationParams as _OfflineKeyRegistrationParams,
+  type OnlineKeyRegistrationParams as _OnlineKeyRegistrationParams,
+  type AppCallMethodCall as _AppCallMethodCall,
+  type AppCreateMethodCall as _AppCreateMethodCall,
+  type AppDeleteMethodCall as _AppDeleteMethodCall,
+  type AppMethodCall as _AppMethodCall,
+  type AppMethodCallTransactionArgument as _AppMethodCallTransactionArgument,
+  type AppUpdateMethodCall as _AppUpdateMethodCall,
+  type ProcessedAppCallMethodCall as _ProcessedAppCallMethodCall,
+  type ProcessedAppCreateMethodCall as _ProcessedAppCreateMethodCall,
+  type ProcessedAppUpdateMethodCall as _ProcessedAppUpdateMethodCall,
+  type PaymentParams as _PaymentParams,
+  type SkipSignaturesSimulateOptions as _SkipSignaturesSimulateOptions,
+  type RawSimulateOptions as _RawSimulateOptions,
+  type SimulateOptions as _SimulateOptions,
+  type ErrorTransformer as _ErrorTransformer,
+  type TransactionComposerConfig as _TransactionComposerConfig,
+  type TransactionComposerParams as _TransactionComposerParams,
+  type BuiltTransactions as _BuiltTransactions,
+  TransactionComposer as _TransactionComposer,
+} from '../composer'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppCallParams = _AppCallParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppCreateParams = _AppCreateParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppDeleteParams = _AppDeleteParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppMethodCallParams = _AppMethodCallParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppUpdateParams = _AppUpdateParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type CommonAppCallParams = _CommonAppCallParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AssetConfigParams = _AssetConfigParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AssetCreateParams = _AssetCreateParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AssetDestroyParams = _AssetDestroyParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AssetFreezeParams = _AssetFreezeParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AssetOptInParams = _AssetOptInParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AssetOptOutParams = _AssetOptOutParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AssetTransferParams = _AssetTransferParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type CommonTransactionParams = _CommonTransactionParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type OfflineKeyRegistrationParams = _OfflineKeyRegistrationParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type OnlineKeyRegistrationParams = _OnlineKeyRegistrationParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppCallMethodCall = _AppCallMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppCreateMethodCall = _AppCreateMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppDeleteMethodCall = _AppDeleteMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppMethodCall = _AppMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppMethodCallTransactionArgument = _AppMethodCallTransactionArgument
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type AppUpdateMethodCall = _AppUpdateMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type ProcessedAppCallMethodCall = _ProcessedAppCallMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type ProcessedAppCreateMethodCall = _ProcessedAppCreateMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type ProcessedAppUpdateMethodCall = _ProcessedAppUpdateMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type PaymentParams = _PaymentParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type SkipSignaturesSimulateOptions = _SkipSignaturesSimulateOptions
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type RawSimulateOptions = _RawSimulateOptions
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type SimulateOptions = _SimulateOptions
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type ErrorTransformer = _ErrorTransformer
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type TransactionComposerConfig = _TransactionComposerConfig
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type TransactionComposerParams = _TransactionComposerParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type BuiltTransactions = _BuiltTransactions
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export const TransactionComposer = _TransactionComposer
+/** @deprecated Import from `@algorandfoundation/algokit-utils/composer` instead */
+export type TransactionComposer = _TransactionComposer
