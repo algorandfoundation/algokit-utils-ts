4aa90694ab63a884a5b5a224b85d6e2dd672ea1b
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 16:49:56 +1000

    chore: add deprecated re-exports for app-deployer in types

---
+++ src/types/app-deployer.ts
@@ -0,0 +1,43 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/app-deployer'
+
+import {
+  type DeployAppUpdateParams as _DeployAppUpdateParams,
+  type DeployAppUpdateMethodCall as _DeployAppUpdateMethodCall,
+  type DeployAppDeleteParams as _DeployAppDeleteParams,
+  type DeployAppDeleteMethodCall as _DeployAppDeleteMethodCall,
+  type AppDeployParams as _AppDeployParams,
+  type AppMetadata as _AppMetadata,
+  type AppLookup as _AppLookup,
+  type AppDeployResult as _AppDeployResult,
+  AppDeployer as _AppDeployer,
+} from '../app-deployer'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type DeployAppUpdateParams = _DeployAppUpdateParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type DeployAppUpdateMethodCall = _DeployAppUpdateMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type DeployAppDeleteParams = _DeployAppDeleteParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type DeployAppDeleteMethodCall = _DeployAppDeleteMethodCall
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type AppDeployParams = _AppDeployParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type AppMetadata = _AppMetadata
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type AppLookup = _AppLookup
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type AppDeployResult = _AppDeployResult
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export const AppDeployer = _AppDeployer
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-deployer` instead */
+export type AppDeployer = _AppDeployer
