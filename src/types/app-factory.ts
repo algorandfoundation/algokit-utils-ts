bf78a5358f06a38c8de62bc7927b5c90809d5d9d
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 16:51:30 +1000

    chore: add deprecated re-exports for app-factory in types

---
+++ src/types/app-factory.ts
@@ -0,0 +1,43 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/app-factory'
+
+import {
+  type AppFactoryParams as _AppFactoryParams,
+  type CreateOnComplete as _CreateOnComplete,
+  type CreateSchema as _CreateSchema,
+  type AppFactoryCreateParams as _AppFactoryCreateParams,
+  type AppFactoryCreateMethodCallParams as _AppFactoryCreateMethodCallParams,
+  type AppFactoryAppClientParams as _AppFactoryAppClientParams,
+  type AppFactoryResolveAppClientByCreatorAndNameParams as _AppFactoryResolveAppClientByCreatorAndNameParams,
+  type AppFactoryDeployParams as _AppFactoryDeployParams,
+  AppFactory as _AppFactory,
+} from '../app-factory'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type AppFactoryParams = _AppFactoryParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type CreateOnComplete = _CreateOnComplete
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type CreateSchema = _CreateSchema
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type AppFactoryCreateParams = _AppFactoryCreateParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type AppFactoryCreateMethodCallParams = _AppFactoryCreateMethodCallParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type AppFactoryAppClientParams = _AppFactoryAppClientParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type AppFactoryResolveAppClientByCreatorAndNameParams = _AppFactoryResolveAppClientByCreatorAndNameParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type AppFactoryDeployParams = _AppFactoryDeployParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export const AppFactory = _AppFactory
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-factory` instead */
+export type AppFactory = _AppFactory
