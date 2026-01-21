c0848fe6bc133bbbea805fd9cecf868815caa295
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 16:16:08 +1000

    chore: add deprecated re-exports for app-client in types

---
+++ src/types/app-client.ts
@@ -0,0 +1,117 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/app-client'
+
+import {
+  AppClient as _AppClient,
+  type AppClientBareCallParams as _AppClientBareCallParams,
+  type AppClientCallABIArgs as _AppClientCallABIArgs,
+  type AppClientCallArgs as _AppClientCallArgs,
+  type AppClientCallCoreParams as _AppClientCallCoreParams,
+  type AppClientCallParams as _AppClientCallParams,
+  type AppClientCallRawArgs as _AppClientCallRawArgs,
+  type AppClientClearStateParams as _AppClientClearStateParams,
+  type AppClientCompilationParams as _AppClientCompilationParams,
+  type AppClientCompilationResult as _AppClientCompilationResult,
+  type AppClientCreateOnComplete as _AppClientCreateOnComplete,
+  type AppClientCreateParams as _AppClientCreateParams,
+  type AppClientDeployCallInterfaceParams as _AppClientDeployCallInterfaceParams,
+  type AppClientDeployCoreParams as _AppClientDeployCoreParams,
+  type AppClientDeployParams as _AppClientDeployParams,
+  type AppClientMethodCallParams as _AppClientMethodCallParams,
+  type AppClientParams as _AppClientParams,
+  type AppClientUpdateParams as _AppClientUpdateParams,
+  type AppDetails as _AppDetails,
+  type AppDetailsBase as _AppDetailsBase,
+  type AppSourceMaps as _AppSourceMaps,
+  type AppSpecAppDetails as _AppSpecAppDetails,
+  type AppSpecAppDetailsBase as _AppSpecAppDetailsBase,
+  type AppSpecAppDetailsByCreatorAndName as _AppSpecAppDetailsByCreatorAndName,
+  type AppSpecAppDetailsById as _AppSpecAppDetailsById,
+  type CallOnComplete as _CallOnComplete,
+  type CloneAppClientParams as _CloneAppClientParams,
+  type FundAppAccountParams as _FundAppAccountParams,
+  type FundAppParams as _FundAppParams,
+  type ResolveAppByCreatorAndName as _ResolveAppByCreatorAndName,
+  type ResolveAppByCreatorAndNameBase as _ResolveAppByCreatorAndNameBase,
+  type ResolveAppById as _ResolveAppById,
+  type ResolveAppByIdBase as _ResolveAppByIdBase,
+  type ResolveAppClientByCreatorAndName as _ResolveAppClientByCreatorAndName,
+  type ResolveAppClientByNetwork as _ResolveAppClientByNetwork,
+  type SourceMapExport as _SourceMapExport,
+} from '../app-client'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export const AppClient = _AppClient
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClient = _AppClient
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientBareCallParams = _AppClientBareCallParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCallABIArgs = _AppClientCallABIArgs
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCallArgs = _AppClientCallArgs
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCallCoreParams = _AppClientCallCoreParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCallParams = _AppClientCallParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCallRawArgs = _AppClientCallRawArgs
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientClearStateParams = _AppClientClearStateParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCompilationParams = _AppClientCompilationParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCompilationResult = _AppClientCompilationResult
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCreateOnComplete = _AppClientCreateOnComplete
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientCreateParams = _AppClientCreateParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientDeployCallInterfaceParams = _AppClientDeployCallInterfaceParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientDeployCoreParams = _AppClientDeployCoreParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientDeployParams = _AppClientDeployParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientMethodCallParams = _AppClientMethodCallParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientParams = _AppClientParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppClientUpdateParams = _AppClientUpdateParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppDetails = _AppDetails
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppDetailsBase = _AppDetailsBase
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppSourceMaps = _AppSourceMaps
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppSpecAppDetails = _AppSpecAppDetails
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppSpecAppDetailsBase = _AppSpecAppDetailsBase
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppSpecAppDetailsByCreatorAndName = _AppSpecAppDetailsByCreatorAndName
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type AppSpecAppDetailsById = _AppSpecAppDetailsById
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type CallOnComplete = _CallOnComplete
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type CloneAppClientParams = _CloneAppClientParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type FundAppAccountParams = _FundAppAccountParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type FundAppParams = _FundAppParams
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type ResolveAppByCreatorAndName = _ResolveAppByCreatorAndName
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type ResolveAppByCreatorAndNameBase = _ResolveAppByCreatorAndNameBase
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type ResolveAppById = _ResolveAppById
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type ResolveAppByIdBase = _ResolveAppByIdBase
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type ResolveAppClientByCreatorAndName = _ResolveAppClientByCreatorAndName
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type ResolveAppClientByNetwork = _ResolveAppClientByNetwork
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app-client` instead */
+export type SourceMapExport = _SourceMapExport
