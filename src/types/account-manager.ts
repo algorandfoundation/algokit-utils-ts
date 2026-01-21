88fe737f9e0c2d4ce9837dd0c33320b52390cd11
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 16:19:13 +1000

    chore: add deprecated re-exports for account-manager in types

---
+++ src/types/account-manager.ts
@@ -0,0 +1,19 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/account-manager'
+
+import {
+  AccountManager as _AccountManager,
+  type EnsureFundedResult as _EnsureFundedResult,
+  getAccountTransactionSigner as _getAccountTransactionSigner,
+} from '../account-manager'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/account-manager` instead */
+export const AccountManager = _AccountManager
+/** @deprecated Import from `@algorandfoundation/algokit-utils/account-manager` instead */
+export type AccountManager = _AccountManager
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/account-manager` instead */
+export type EnsureFundedResult = _EnsureFundedResult
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/account-manager` instead */
+export const getAccountTransactionSigner = _getAccountTransactionSigner
