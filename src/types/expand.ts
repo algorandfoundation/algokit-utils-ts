e764cf5a13d286c824c90b5f4a8342b3ea85ed1c
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 17:08:56 +1000

    chore: add deprecated re-exports for expand in types

---
+++ src/types/expand.ts
@@ -0,0 +1,7 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/expand'
+
+import { type Expand as _Expand } from '../expand'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/expand` instead */
+export type Expand<T> = _Expand<T>
