4767d45c4e7624184a754a117131a669ccafd758
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 16:36:20 +1000

    chore: add deprecated re-exports for algorand-client-transaction-sender in types

---
+++ src/types/algorand-client-transaction-sender.ts
@@ -0,0 +1,9 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/algorand-client-transaction-sender'
+
+import { AlgorandClientTransactionSender as _AlgorandClientTransactionSender } from '../algorand-client-transaction-sender'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/algorand-client-transaction-sender` instead */
+export const AlgorandClientTransactionSender = _AlgorandClientTransactionSender
+/** @deprecated Import from `@algorandfoundation/algokit-utils/algorand-client-transaction-sender` instead */
+export type AlgorandClientTransactionSender = _AlgorandClientTransactionSender
