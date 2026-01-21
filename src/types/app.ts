db2a92ec3c006185ae0362edacd784d87a61b5c6
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 17:00:51 +1000

    chore: add deprecated re-exports for app in types

---
+++ src/types/app.ts
@@ -0,0 +1,133 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/app'
+
+import {
+  UPDATABLE_TEMPLATE_NAME as _UPDATABLE_TEMPLATE_NAME,
+  DELETABLE_TEMPLATE_NAME as _DELETABLE_TEMPLATE_NAME,
+  APP_DEPLOY_NOTE_DAPP as _APP_DEPLOY_NOTE_DAPP,
+  APP_PAGE_MAX_SIZE as _APP_PAGE_MAX_SIZE,
+  ABI_RETURN_PREFIX as _ABI_RETURN_PREFIX,
+  OnUpdate as _OnUpdate,
+  OnSchemaBreak as _OnSchemaBreak,
+  type AppReference as _AppReference,
+  type CoreAppCallArgs as _CoreAppCallArgs,
+  type RawAppCallArgs as _RawAppCallArgs,
+  type ABIAppCallArg as _ABIAppCallArg,
+  type ABIAppCallArgs as _ABIAppCallArgs,
+  type AppCallArgs as _AppCallArgs,
+  type AppCallParams as _AppCallParams,
+  type AppStorageSchema as _AppStorageSchema,
+  type CompiledTeal as _CompiledTeal,
+  type AppCallTransactionResultOfType as _AppCallTransactionResultOfType,
+  type AppCallTransactionResult as _AppCallTransactionResult,
+  type AppDeployMetadata as _AppDeployMetadata,
+  type AppMetadata as _AppMetadata,
+  type AppLookup as _AppLookup,
+  type TealTemplateParams as _TealTemplateParams,
+  type AppCompilationResult as _AppCompilationResult,
+  type AppReturn as _AppReturn,
+  type SendAppTransactionResult as _SendAppTransactionResult,
+  type SendAppUpdateTransactionResult as _SendAppUpdateTransactionResult,
+  type SendAppCreateTransactionResult as _SendAppCreateTransactionResult,
+  type AppState as _AppState,
+  type BoxName as _BoxName,
+  type BoxValueRequestParams as _BoxValueRequestParams,
+  type BoxValuesRequestParams as _BoxValuesRequestParams,
+} from '../app'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export const UPDATABLE_TEMPLATE_NAME = _UPDATABLE_TEMPLATE_NAME
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export const DELETABLE_TEMPLATE_NAME = _DELETABLE_TEMPLATE_NAME
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export const APP_DEPLOY_NOTE_DAPP = _APP_DEPLOY_NOTE_DAPP
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export const APP_PAGE_MAX_SIZE = _APP_PAGE_MAX_SIZE
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export const ABI_RETURN_PREFIX = _ABI_RETURN_PREFIX
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export const OnUpdate = _OnUpdate
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type OnUpdate = _OnUpdate
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export const OnSchemaBreak = _OnSchemaBreak
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type OnSchemaBreak = _OnSchemaBreak
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppReference = _AppReference
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type CoreAppCallArgs = _CoreAppCallArgs
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type RawAppCallArgs = _RawAppCallArgs
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type ABIAppCallArg = _ABIAppCallArg
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type ABIAppCallArgs = _ABIAppCallArgs
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppCallArgs = _AppCallArgs
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppCallParams = _AppCallParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppStorageSchema = _AppStorageSchema
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type CompiledTeal = _CompiledTeal
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppCallTransactionResultOfType<T> = _AppCallTransactionResultOfType<T>
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppCallTransactionResult = _AppCallTransactionResult
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppDeployMetadata = _AppDeployMetadata
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppMetadata = _AppMetadata
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppLookup = _AppLookup
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type TealTemplateParams = _TealTemplateParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppCompilationResult = _AppCompilationResult
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppReturn<TReturn> = _AppReturn<TReturn>
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type SendAppTransactionResult = _SendAppTransactionResult
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type SendAppUpdateTransactionResult = _SendAppUpdateTransactionResult
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type SendAppCreateTransactionResult = _SendAppCreateTransactionResult
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type AppState = _AppState
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type BoxName = _BoxName
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type BoxValueRequestParams = _BoxValueRequestParams
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/app` instead */
+export type BoxValuesRequestParams = _BoxValuesRequestParams
