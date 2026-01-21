a2dbf4f6043583260038daea34b7219fcbb87b1d
Hoang Dinh <patrick.dinh@makerx.com.au>
2026-01-21 17:09:29 +1000

    chore: add deprecated re-exports for instance-of in types

---
+++ src/types/instance-of.ts
@@ -0,0 +1,7 @@
+// Re-exports with deprecation notices for backwards compatibility
+// New imports should use '@algorandfoundation/algokit-utils/instance-of'
+
+import { type InterfaceOf as _InterfaceOf } from '../instance-of'
+
+/** @deprecated Import from `@algorandfoundation/algokit-utils/instance-of` instead */
+export type InterfaceOf<ClassType> = _InterfaceOf<ClassType>
