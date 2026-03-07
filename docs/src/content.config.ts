9a85503524e05337badd22257c14bffaa547884b
David Rojas <lempira@gmail.com>
2026-03-07 11:15:05 -0500

    chore: removed obsolete code

--- docs/src/content.config.ts
+++ docs/src/content.config.ts
@@ -1,23 +1,7 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
 import { docsLoader } from '@astrojs/starlight/loaders';
 import { docsSchema } from '@astrojs/starlight/schema';
-// import { examplesLoader } from './loaders/examples-loader';
 
 export const collections = {
 	docs: defineCollection({ loader: docsLoader(), schema: docsSchema() }),
-	// examples: defineCollection({
-	// 	loader: examplesLoader(),
-	// 	schema: z.object({
-	// 		id: z.string(),
-	// 		title: z.string(),
-	// 		description: z.string(),
-	// 		prerequisites: z.string(),
-	// 		code: z.string(),
-	// 		category: z.string(),
-	// 		categoryLabel: z.string(),
-	// 		order: z.number(),
-	// 		filename: z.string(),
-	// 		runCommand: z.string(),
-	// 	}),
-	// }),
 };
