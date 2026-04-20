4d9b3cfbe53b00a6fd8b76695e46fed73d166a10
Joe Polny <joepolny@gmail.com>
2026-04-20 16:46:54 -0400

    test: wallet-utils tests

--- tests/wallet_utils/epic_ak_1000.test.ts
+++ tests/wallet_utils/epic_ak_1000.test.ts
@@ -1,13 +1,21 @@
-import { expect, test, describe } from "vitest";
+import { test, describe, beforeAll, expect } from 'vitest'
+import { balance, deriveHdAccountsFromMnemonic, getAccountAssets, getAssetBalance, getAssetInfo } from './common'
+import { AlgorandClient, microAlgos } from '../../src'
 
-describe("Epic AK-1000", () => {
+describe('Epic AK-1000', () => {
+  let algorand: AlgorandClient
+
+  beforeAll(async () => {
+    algorand = AlgorandClient.defaultLocalNet()
+  })
+
   // Polytest Suite: Epic AK-1000
 
-  describe("AK-1000 Stories", () => {
+  describe('AK-1000 Stories', () => {
     // Polytest Group: AK-1000 Stories
 
-    /* 
-      AK-996: View only wallet. As a user I want to add a public address to my wallet to view it’s balance (and make transactions)
+    /*
+      AK-996: View only wallet. As a user I want to add a public address to my wallet to view it's balance (and make transactions)
 
       Acceptance Criteria:
       Given I have a public key
@@ -22,11 +30,18 @@ describe("Epic AK-1000", () => {
 
       (for hardware wallets)
      */
-    test("AK-996", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-996', async () => {
+      const [account] = await deriveHdAccountsFromMnemonic({ numAccounts: 1 })
 
-    /* 
+      await algorand.account.ensureFundedFromEnvironment(account.addr, microAlgos(1_000_000))
+
+      expect(await balance(algorand, account)).toBe(1_100_000n)
+
+      const payTxn = await algorand.createTransaction.payment({ sender: account, receiver: account, amount: microAlgos(1) })
+      expect(payTxn.sender.toString()).toBe(account.addr.toString())
+    })
+
+    /*
       AK-1006: Current balance. As a user I want to view my balance quickly so that I know how much algo I have
 
       Acceptance Criteria:
@@ -36,11 +51,36 @@ describe("Epic AK-1000", () => {
 
       And I can see how much Algo I have in my wallet
      */
-    test("AK-1006", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-1006', async () => {
+      const accounts = await deriveHdAccountsFromMnemonic({ numAccounts: 20 })
+      const fundedIndices = [0, 5, 10]
+      const amounts = [1_000_000n, 500_000n, 250_000n]
 
-    /* 
+      for (let i = 0; i < fundedIndices.length; i++) {
+        await algorand.account.ensureFundedFromEnvironment(accounts[fundedIndices[i]].addr, microAlgos(amounts[i]))
+      }
+
+      let walletTotal = 0n
+      for (const account of accounts) {
+        const accountBalance = await balance(algorand, account)
+        walletTotal += accountBalance
+      }
+
+      for (let i = 0; i < fundedIndices.length; i++) {
+        const accountBalance = await balance(algorand, accounts[fundedIndices[i]])
+        expect(accountBalance).toBeGreaterThanOrEqual(amounts[i])
+      }
+
+      for (let i = 0; i < accounts.length; i++) {
+        if (fundedIndices.includes(i)) continue
+        expect(await balance(algorand, accounts[i])).toBe(0n)
+      }
+
+      const expectedTotal = (await Promise.all(accounts.map((a) => balance(algorand, a)))).reduce((a, b) => a + b, 0n)
+      expect(walletTotal).toBe(expectedTotal)
+    })
+
+    /*
       AK-1007: Current ASA balance. As a user I want to see my ASA balance quickly so I know what other currencies are in my wallet
 
       Acceptance Criteria:
@@ -50,11 +90,59 @@ describe("Epic AK-1000", () => {
 
       And I can see how much ASA I have in my wallet
      */
-    test("AK-1007", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-1007', async () => {
+      const accounts = await deriveHdAccountsFromMnemonic({ numAccounts: 20 })
+      const creator = accounts[0]
 
-    /* 
+      await algorand.account.ensureFundedFromEnvironment(creator.addr, microAlgos(2_000_000))
+
+      const assetResult = await algorand.send.assetCreate({
+        sender: creator,
+        total: 1_000_000n,
+        decimals: 2,
+        assetName: 'Test ASA',
+        unitName: 'TASA',
+        url: 'https://example.com/asa',
+      })
+      const assetId = assetResult.assetId
+
+      const recipients = [accounts[1], accounts[2], accounts[3]]
+      const transferAmounts = [100n, 250n, 500n]
+
+      for (const recipient of recipients) {
+        await algorand.account.ensureFundedFromEnvironment(recipient.addr, microAlgos(500_000))
+      }
+
+      for (const recipient of recipients) {
+        await algorand.send.assetOptIn({ sender: recipient, assetId })
+      }
+
+      for (let i = 0; i < recipients.length; i++) {
+        await algorand.send.assetTransfer({
+          sender: creator,
+          receiver: recipients[i],
+          assetId,
+          amount: transferAmounts[i],
+        })
+      }
+
+      let walletTotal = 0n
+      for (const account of accounts) {
+        const assetBalance = await getAssetBalance(algorand, account, assetId)
+        walletTotal += assetBalance
+      }
+
+      expect(await getAssetBalance(algorand, accounts[0], assetId)).toBe(1_000_000n - 850n)
+      expect(await getAssetBalance(algorand, accounts[1], assetId)).toBe(100n)
+      expect(await getAssetBalance(algorand, accounts[2], assetId)).toBe(250n)
+      expect(await getAssetBalance(algorand, accounts[3], assetId)).toBe(500n)
+      for (let i = 4; i < accounts.length; i++) {
+        expect(await getAssetBalance(algorand, accounts[i], assetId)).toBe(0n)
+      }
+      expect(walletTotal).toBe(1_000_000n)
+    })
+
+    /*
       AK-1008: Current NFT balance. As a user I want to see my NFTs quickly so I know what NFTS are in my wallet
 
       Acceptance Criteria:
@@ -66,11 +154,90 @@ describe("Epic AK-1000", () => {
 
       And its metadata
      */
-    test("AK-1008", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-1008', async () => {
+      const accounts = await deriveHdAccountsFromMnemonic({ numAccounts: 20 })
+      const creator = accounts[0]
 
-    /* 
+      await algorand.account.ensureFundedFromEnvironment(creator.addr, microAlgos(2_000_000))
+
+      const nfts: { assetId: bigint; assetName: string; url: string; metadataHash: Uint8Array }[] = []
+      for (let i = 0; i < 3; i++) {
+        const hash = new Uint8Array(32).fill(i + 1)
+        const result = await algorand.send.assetCreate({
+          sender: creator,
+          total: 1n,
+          decimals: 0,
+          assetName: `NFT ${i + 1}`,
+          unitName: `NFT${i + 1}`,
+          url: `https://example.com/nft/${i + 1}`,
+          metadataHash: hash,
+        })
+        nfts.push({
+          assetId: result.assetId,
+          assetName: `NFT ${i + 1}`,
+          url: `https://example.com/nft/${i + 1}`,
+          metadataHash: hash,
+        })
+      }
+
+      const recipients = [accounts[1], accounts[2], accounts[3]]
+      for (const recipient of recipients) {
+        await algorand.account.ensureFundedFromEnvironment(recipient.addr, microAlgos(500_000))
+      }
+
+      for (let i = 0; i < recipients.length; i++) {
+        await algorand.send.assetOptIn({ sender: recipients[i], assetId: nfts[i].assetId })
+      }
+
+      for (let i = 0; i < recipients.length; i++) {
+        await algorand.send.assetTransfer({
+          sender: creator,
+          receiver: recipients[i],
+          assetId: nfts[i].assetId,
+          amount: 1n,
+        })
+      }
+
+      let walletNftTotal = 0
+      for (const account of accounts) {
+        const assets = await getAccountAssets(algorand, account)
+        const nftHoldings = []
+        for (const holding of assets) {
+          if (holding.amount === 0n) continue
+          const assetInfo = await getAssetInfo(algorand, holding.assetId)
+          if (assetInfo.total === 1n && assetInfo.decimals === 0) {
+            nftHoldings.push({ ...holding, ...assetInfo })
+          }
+        }
+
+        if (recipients.some((r) => r.addr.toString() === account.addr.toString())) {
+          expect(nftHoldings.length).toBe(1)
+          walletNftTotal += 1
+        } else {
+          expect(nftHoldings.length).toBe(0)
+        }
+      }
+
+      expect(walletNftTotal).toBe(3)
+
+      for (let i = 0; i < recipients.length; i++) {
+        const assets = await getAccountAssets(algorand, recipients[i])
+        const nftAssets = []
+        for (const holding of assets) {
+          if (holding.amount === 0n) continue
+          const assetInfo = await getAssetInfo(algorand, holding.assetId)
+          if (assetInfo.total === 1n && assetInfo.decimals === 0) {
+            nftAssets.push(assetInfo)
+          }
+        }
+        expect(nftAssets.length).toBe(1)
+        expect(nftAssets[0].assetName).toBe(nfts[i].assetName)
+        expect(nftAssets[0].url).toBe(nfts[i].url)
+        expect(nftAssets[0].metadataHash).toEqual(nfts[i].metadataHash)
+      }
+    })
+
+    /*
       AK-1009: View smart contract asset. As a user I want to know how much ARC20 assets I have so I know what I have in my wallet
 
       Acceptance Criteria:
@@ -84,10 +251,8 @@ describe("Epic AK-1000", () => {
 
       ARC20 → ARC84
      */
-    test("AK-1009", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
-
-  });
-
-});
+    test.skip('AK-1009', () => {
+      throw new Error('TEST NOT IMPLEMENTED')
+    })
+  })
+})
--- tests/wallet_utils/epic_ak_1002.test.ts
+++ tests/wallet_utils/epic_ak_1002.test.ts
@@ -1,95 +1,176 @@
-import { expect, test, describe } from "vitest";
+import { expect, test, describe, beforeAll } from 'vitest'
+import { deriveHdAccountsFromMnemonic } from './common'
+import { AlgorandClient, microAlgos } from '../../src'
+import { encodeTransactionRaw, decodeTransaction, bytesForSigning, TransactionSigner } from '@algorandfoundation/algokit-transact'
+import nacl from 'tweetnacl'
 
-describe("Epic AK-1002", () => {
+describe('Epic AK-1002', () => {
+  let algorand: AlgorandClient
+
+  beforeAll(async () => {
+    algorand = AlgorandClient.defaultLocalNet()
+  })
+
   // Polytest Suite: Epic AK-1002
 
-  describe("AK-1002 Stories", () => {
+  describe('AK-1002 Stories', () => {
     // Polytest Group: AK-1002 Stories
 
-    /* 
+    /*
       AK-1018: Review transaction. As a user I want to review a transaction so I know that its safe to sign it
 
       Acceptance Criteria:
       Given I have a well formed transaction
-
       Then I can check variables like:
-
       # What assets are being send
       # Where they are sent to
       # Any meta data in the Tx
       # From which address its send
       # potential ramifications/understanding side effects like losing rekey
      */
-    test("AK-1018", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-1018', async () => {
+      const accounts = await deriveHdAccountsFromMnemonic({ numAccounts: 2 })
+      const [sender, receiver] = accounts
+      const note = new TextEncoder().encode('test-metadata')
+      const rekeyAddress = receiver.addr
 
-    /* 
+      const payTxn = await algorand.createTransaction.payment({
+        sender,
+        receiver,
+        amount: microAlgos(100_000),
+        note,
+        rekeyTo: rekeyAddress,
+      })
+
+      expect(payTxn.sender.toString()).toBe(sender.addr.toString())
+      expect(payTxn.payment?.receiver.toString()).toBe(receiver.addr.toString())
+      expect(payTxn.payment?.amount).toBe(100_000n)
+      expect(payTxn.note).toEqual(note)
+      expect(payTxn.rekeyTo?.toString()).toBe(rekeyAddress.toString())
+
+      const assetTxn = await algorand.createTransaction.assetTransfer({
+        sender,
+        receiver,
+        assetId: 123n,
+        amount: 50n,
+      })
+
+      expect(assetTxn.sender.toString()).toBe(sender.addr.toString())
+      expect(assetTxn.assetTransfer?.receiver.toString()).toBe(receiver.addr.toString())
+      expect(assetTxn.assetTransfer?.assetId).toBe(123n)
+      expect(assetTxn.assetTransfer?.amount).toBe(50n)
+    })
+
+    /*
       AK-1019: Sign transaction. As a user I want to sign a transaction so it can be executed on the blockchain
 
       Acceptance Criteria:
       Given I have reviewed my transaction
-
       Then I can sign the transaction
-
       And share with thirdparty (dapp)
      */
-    test("AK-1019", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-1019', async () => {
+      const accounts = await deriveHdAccountsFromMnemonic({ numAccounts: 2 })
+      const [sender, receiver] = accounts
 
-    /* 
+      await algorand.account.ensureFundedFromEnvironment(sender.addr, microAlgos(1_000_000))
+
+      const txn = await algorand.createTransaction.payment({
+        sender,
+        receiver,
+        amount: microAlgos(100_000),
+      })
+
+      const result = await algorand.send.newGroup().addTransaction(txn, sender.signer).send()
+
+      expect(result.confirmations.length).toBe(1)
+      expect(result.confirmations[0].confirmedRound).toBeGreaterThan(0n)
+    })
+
+    /*
       AK-1020: Hardware sign transaction. As a user I want to share my unsigned transaction with a HSM so that they can sign it on my behalf
 
       Acceptance Criteria:
       Given I have review the transaction
-
       Then I can connect to a hardware module like Ledger or HashiCorp
-
       And get it to sign my transaction
-
       Then Submit it to the blockchain
      */
-    test("AK-1020", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-1020', async () => {
+      const accounts = await deriveHdAccountsFromMnemonic({ numAccounts: 2 })
+      const [sender, receiver] = accounts
 
-    /* 
+      await algorand.account.ensureFundedFromEnvironment(sender.addr, microAlgos(1_000_000))
+
+      const txn = await algorand.createTransaction.payment({
+        sender,
+        receiver,
+        amount: microAlgos(100_000),
+      })
+
+      // Simulate an HSM that proxies signing to the account signer
+      const hsmSigner: TransactionSigner = async (txnGroup, indexesToSign) => {
+        return sender.signer(txnGroup, indexesToSign)
+      }
+
+      const result = await algorand.send.newGroup().addTransaction(txn, hsmSigner).send()
+
+      expect(result.confirmations.length).toBe(1)
+      expect(result.confirmations[0].confirmedRound).toBeGreaterThan(0n)
+    })
+
+    /*
       AK-1021: Export unsigned transaction. As a user I want to export an unsigned or partially signed transaction so that someone else can do something with it
 
       Acceptance Criteria:
       Given I have reviewed the transaction
-
       Then I can share the unsigned transaction with Base64 encoding
      */
-    test("AK-1021", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-1021', async () => {
+      const accounts = await deriveHdAccountsFromMnemonic({ numAccounts: 2 })
+      const [sender, receiver] = accounts
+      const note = new TextEncoder().encode('hello-world')
 
-    /* 
+      const txn = await algorand.createTransaction.payment({
+        sender,
+        receiver,
+        amount: microAlgos(1),
+        note,
+      })
+
+      const encoded = encodeTransactionRaw(txn)
+      const base64 = Buffer.from(encoded).toString('base64')
+      const decodedBack = decodeTransaction(new Uint8Array(Buffer.from(base64, 'base64')))
+
+      expect(decodedBack.sender.toString()).toBe(sender.addr.toString())
+      expect(decodedBack.payment?.receiver.toString()).toBe(receiver.addr.toString())
+      expect(decodedBack.payment?.amount).toBe(1n)
+      expect(decodedBack.note).toEqual(note)
+    })
+
+    /*
       AK-1248: sign arbitrary data. As a user I want to proof that I own a certain key, so I can get access to a service.
 
       Acceptance Criteria:
       Given I have setup a wallet
-
       And I contect to a service
-
       And the service requests me to sign a nounce with a specific key
-
       Then I can sign that nounce and return result
-
-
-
-      examples:
-
-      [https://docs.metamask.io/wallet/how-to/sign-data/siwe/|https://docs.metamask.io/wallet/how-to/sign-data/siwe/|smart-link] 
-
-      [https://cips.cardano.org/cip/CIP-0008|https://cips.cardano.org/cip/CIP-0008|smart-link]
      */
-    test("AK-1248", () => {
-      throw new Error("TEST NOT IMPLEMENTED");
-    });
+    test('AK-1248', async () => {
+      const accounts = await deriveHdAccountsFromMnemonic({ numAccounts: 1 })
+      const account = accounts[0]
 
-  });
+      const nonce = new TextEncoder().encode('service-nonce-12345')
+      const signature = await account.mxBytesSigner(nonce)
 
-});
+      expect(signature).toBeInstanceOf(Uint8Array)
+      expect(signature.length).toBe(64)
+
+      const bytesToSign = bytesForSigning.mxBytes(nonce)
+      const isValid = nacl.sign.detached.verify(bytesToSign, signature, account.addr.publicKey)
+
+      expect(isValid).toBe(true)
+    })
+  })
+})
